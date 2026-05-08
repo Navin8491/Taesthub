c2da774a6b15b691a38bfb3b6727633f4e9dae79
Navin8491 <navin8491@gmail.com>
2026-05-08 14:32:19 +0530

    feat: improved mobile navbar layout and visibility

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -32,6 +32,98 @@ const Navbar = () => {
             <span>TasteHub</span>
           </Link>
 
+          <div className="user_option order-lg-3">
+            <Link to="" className="user_link" onClick={() => setIsMenuOpen(false)}>
+              <i className="fa fa-user" aria-hidden="true"></i>
+            </Link>
+            <Link 
+              className="cart_link" 
+              to="/cart"
+              style={{ position: 'relative' }}
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style={{ enableBackground: 'new 0 0 456.029 456.029' }} xmlSpace="preserve">
+                <g>
+                  <g>
+                    <path d="M345.6,338.862c-29.184,0-53.248,23.552-53.248,53.248c0,29.184,23.552,53.248,53.248,53.248 c29.184,0,53.248-23.552,53.248-53.248C398.336,362.926,374.784,338.862,345.6,338.862z" />
+                  </g>
+                </g>
+                <g>
+                  <g>
+                    <path d="M439.296,84.91c-1.024,0-2.56-0.512-4.096-0.512H112.64l-5.12-34.304C104.448,27.566,84.992,10.67,61.952,10.67H20.48 C9.216,10.67,0,19.886,0,31.15c0,11.264,9.216,20.48,20.48,20.48h41.472c2.56,0,4.608,2.048,5.12,4.608l31.744,216.064 c4.096,27.136,27.648,47.616,55.296,47.616h212.992c26.624,0,49.664-18.944,55.296-45.056l33.28-166.4 C457.728,97.71,450.56,86.958,439.296,84.91z" />
+                  </g>
+                </g>
+                <g>
+                  <g>
+                    <path d="M215.04,389.55c-1.024-28.16-24.576-50.688-52.736-50.688c-29.696,1.536-52.224,26.112-51.2,55.296 c1.024,28.16,24.064,50.688,52.224,50.688h1.024C193.536,443.31,216.576,418.734,215.04,389.55z" />
+                  </g>
+                </g>
+              </svg>
+              {cartCount > 0 && (
+                <span style={{ position: 'absolute', top: '-8px', right: '-8px', background: '#ffbe33', color: '#fff', borderRadius: '50%', padding: '2px 6px', fontSize: '10px', fontWeight: 'bold' }}>
+                  {cartCount}
+                </span>
+              )}
+            </Link>
+            <form 
+              className="form-inline" 
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (searchQuery.trim()) {
+                  setIsMenuOpen(false);
+                  navigate('/menu');
+                  setTimeout(() => {
+                    const menuSection = document.querySelector('.food_section');
+                    if (menuSection) menuSection.scrollIntoView({ behavior: 'smooth' });
+                  }, 100);
+                } else {
+                  setIsSearchOpen(!isSearchOpen);
+                }
+              }}
+              style={{ display: 'flex', alignItems: 'center' }}
+            >
+              <AnimatePresence>
+                {isSearchOpen && (
+                  <motion.input
+                    initial={{ width: 0, opacity: 0 }}
+                    animate={{ width: 150, opacity: 1 }}
+                    exit={{ width: 0, opacity: 0 }}
+                    transition={{ duration: 0.3 }}
+                    type="text"
+                    className="form-control"
+                    placeholder="Search..."
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    style={{ 
+                      marginRight: '10px', 
+                      height: '35px', 
+                      borderRadius: '20px', 
+                      border: '1px solid #ccc',
+                      padding: '0 15px',
+                      outline: 'none'
+                    }}
+                    autoFocus
+                  />
+                )}
+              </AnimatePresence>
+              <button 
+                className="btn my-2 my-sm-0 nav_search-btn" 
+                type="submit"
+                onClick={(e) => {
+                  if (!isSearchOpen) {
+                    e.preventDefault();
+                    setIsSearchOpen(true);
+                  }
+                }}
+              >
+                <i className="fa fa-search" aria-hidden="true"></i>
+              </button>
+            </form>
+            <a href="#" className="order_online" onClick={handleOrderOnline}>
+              Order Online
+            </a>
+          </div>
+
           <button
             className={`navbar-toggler ${isMenuOpen ? '' : 'collapsed'}`}
             type="button"
@@ -58,97 +150,6 @@ const Navbar = () => {
                 <Link className="nav-link" to="/book" onClick={() => setIsMenuOpen(false)}>Book Table</Link>
               </li>
             </ul>
-            <div className="user_option">
-              <Link to="" className="user_link" onClick={() => setIsMenuOpen(false)}>
-                <i className="fa fa-user" aria-hidden="true"></i>
-              </Link>
-              <Link 
-                className="cart_link" 
-                to="/cart"
-                style={{ position: 'relative' }}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style={{ enableBackground: 'new 0 0 456.029 456.029' }} xmlSpace="preserve">
-                  <g>
-                    <g>
-                      <path d="M345.6,338.862c-29.184,0-53.248,23.552-53.248,53.248c0,29.184,23.552,53.248,53.248,53.248 c29.184,0,53.248-23.552,53.248-53.248C398.336,362.926,374.784,338.862,345.6,338.862z" />
-                    </g>
-                  </g>
-                  <g>
-                    <g>
-                      <path d="M439.296,84.91c-1.024,0-2.56-0.512-4.096-0.512H112.64l-5.12-34.304C104.448,27.566,84.992,10.67,61.952,10.67H20.48 C9.216,10.67,0,19.886,0,31.15c0,11.264,9.216,20.48,20.48,20.48h41.472c2.56,0,4.608,2.048,5.12,4.608l31.744,216.064 c4.096,27.136,27.648,47.616,55.296,47.616h212.992c26.624,0,49.664-18.944,55.296-45.056l33.28-166.4 C457.728,97.71,450.56,86.958,439.296,84.91z" />
-                    </g>
-                  </g>
-                  <g>
-                    <g>
-                      <path d="M215.04,389.55c-1.024-28.16-24.576-50.688-52.736-50.688c-29.696,1.536-52.224,26.112-51.2,55.296 c1.024,28.16,24.064,50.688,52.224,50.688h1.024C193.536,443.31,216.576,418.734,215.04,389.55z" />
-                    </g>
-                  </g>
-                </svg>
-                {cartCount > 0 && (
-                  <span style={{ position: 'absolute', top: '-8px', right: '-8px', background: '#ffbe33', color: '#fff', borderRadius: '50%', padding: '2px 6px', fontSize: '10px', fontWeight: 'bold' }}>
-                    {cartCount}
-                  </span>
-                )}
-              </Link>
-              <form 
-                className="form-inline" 
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  if (searchQuery.trim()) {
-                    setIsMenuOpen(false);
-                    navigate('/menu');
-                    setTimeout(() => {
-                      const menuSection = document.querySelector('.food_section');
-                      if (menuSection) menuSection.scrollIntoView({ behavior: 'smooth' });
-                    }, 100);
-                  } else {
-                    setIsSearchOpen(!isSearchOpen);
-                  }
-                }}
-                style={{ display: 'flex', alignItems: 'center' }}
-              >
-                <AnimatePresence>
-                  {isSearchOpen && (
-                    <motion.input
-                      initial={{ width: 0, opacity: 0 }}
-                      animate={{ width: 150, opacity: 1 }}
-                      exit={{ width: 0, opacity: 0 }}
-                      transition={{ duration: 0.3 }}
-                      type="text"
-                      className="form-control"
-                      placeholder="Search..."
-                      value={searchQuery}
-                      onChange={(e) => setSearchQuery(e.target.value)}
-                      style={{ 
-                        marginRight: '10px', 
-                        height: '35px', 
-                        borderRadius: '20px', 
-                        border: '1px solid #ccc',
-                        padding: '0 15px',
-                        outline: 'none'
-                      }}
-                      autoFocus
-                    />
-                  )}
-                </AnimatePresence>
-                <button 
-                  className="btn my-2 my-sm-0 nav_search-btn" 
-                  type="submit"
-                  onClick={(e) => {
-                    if (!isSearchOpen) {
-                      e.preventDefault();
-                      setIsSearchOpen(true);
-                    }
-                  }}
-                >
-                  <i className="fa fa-search" aria-hidden="true"></i>
-                </button>
-              </form>
-              <a href="#" className="order_online" onClick={handleOrderOnline}>
-                Order Online
-              </a>
-            </div>
           </div>
         </nav>
       </div>
